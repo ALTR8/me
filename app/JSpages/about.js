380767c058d1ca795c911d454d1b3b8391dbb13d
Alexandra Roth <alroth8@gmail.com>
2017-05-12 14:43:45 -0400

    things are starting to happen

---
+++ app/JSpages/about.js
@@ -0,0 +1,5 @@
+function aboutRenderer(){
+  var aboutDiv = document.getElementsByClassName("about")
+  var aboutContent = document.createTextNode("This is my about me page")
+  aboutDiv[0].appendChild(aboutContent)
+}
